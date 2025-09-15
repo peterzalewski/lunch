f4b5da1bd007dfa7db3749e75c1810f703e3763e
Peter Zalewski <peter@zalewski.com>
2025-09-14 22:51:34 -0400

    feat: cache menus locally

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module lunch
 
 go 1.25.0
+
+require (
+	github.com/adrg/xdg v0.5.3 // indirect
+	golang.org/x/sys v0.36.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
+// TODO: Log actions
+// TODO: Use cobra for verbose, cache-bust flags
+// TODO: Store config in yaml and read with Viper
+
 import (
 	"bufio"
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 	"time"
+
+	"github.com/adrg/xdg"
 )
 
 type LunchConfig struct {
@@ -24,6 +31,63 @@ func (lc LunchConfig) UrlForOption(lo LunchOption) string {
 	return fmt.Sprintf("%s/%s/%s/%s.csv", lc.basePath, lc.schoolYear, strings.ToLower(now.Month().String()), lo.path)
 }
 
+func (lc LunchConfig) KeyForOption(lo LunchOption) string {
+	now := time.Now()
+	return fmt.Sprintf("%s-%s-%s.csv", lc.schoolYear, strings.ToLower(now.Month().String()), lo.path)
+}
+
+// TODO: Should return []struct of some sort
+func (lc LunchConfig) DataFor(lo LunchOption) (string, error) {
+	var builder strings.Builder
+
+	// Get cache key
+	// If it exists in the cache, return
+	// Else download the file, cache, return
+	cacheKey := lc.KeyForOption(lo)
+	cacheFile, err := xdg.CacheFile(fmt.Sprintf("lunch/%s", cacheKey))
+	if err != nil {
+		return "", err
+	}
+
+	if _, err := os.Stat(cacheFile); err == nil {
+		// TODO: Read and return
+		cache, err := os.Open(cacheFile)
+		if err != nil {
+			return "", err
+		}
+		reader := bufio.NewScanner(cache)
+		for reader.Scan() {
+			// TODO: Here (and elsewhere) don't slurp the newline and then add it back in such a heavyweight way
+			builder.WriteString(fmt.Sprintf("%s\n", reader.Text()))
+		}
+	} else {
+		// TODO: Should probably be its own function
+		lunchUrl := lc.UrlForOption(lo)
+		resp, err := http.Get(lunchUrl)
+		if err != nil {
+			return "", err
+		}
+		defer resp.Body.Close()
+
+		cache, err := os.Create(cacheFile)
+		if err != nil {
+			return "", err
+		}
+
+		reader := bufio.NewScanner(resp.Body)
+		writer := bufio.NewWriter(cache)
+		defer writer.Flush()
+
+		for reader.Scan() {
+			line := reader.Text()
+			builder.WriteString(fmt.Sprintf("%s\n", line))
+			writer.WriteString(fmt.Sprintf("%s\n", line))
+		}
+	}
+
+	return builder.String(), nil
+}
+
 func main() {
 	highSchoolColdLunch := LunchOption{
 		name: "High School Express Cold Lunch Menu",
@@ -35,15 +99,5 @@ func main() {
 		options:    []LunchOption{highSchoolColdLunch},
 	}
 
-	lunchUrl := config.UrlForOption(config.options[0])
-	resp, err := http.Get(lunchUrl)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	scanner := bufio.NewScanner(resp.Body)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
+	fmt.Println(config.DataFor(highSchoolColdLunch))
 }
