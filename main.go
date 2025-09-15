5a4a15e8d987b5b1e091e2d84803e925303f8b50
Peter Zalewski <peter@zalewski.com>
2025-09-14 20:26:01 -0400

    feat: print current month high school cold menu

--- main.go
+++ main.go
@@ -4,14 +4,39 @@ import (
 	"bufio"
 	"fmt"
 	"net/http"
+	"strings"
+	"time"
 )
 
-const (
-	LunchURL = "https://www.schools.nyc.gov/school-life/food/menus/school-lunch-meals"
-)
+type LunchConfig struct {
+	schoolYear string
+	basePath   string
+	options    []LunchOption
+}
 
-func main()  {
-	resp, err := http.Get(LunchURL)
+type LunchOption struct {
+	name string
+	path string
+}
+
+func (lc LunchConfig) UrlForOption(lo LunchOption) string {
+	now := time.Now()
+	return fmt.Sprintf("%s/%s/%s/%s.csv", lc.basePath, lc.schoolYear, strings.ToLower(now.Month().String()), lo.path)
+}
+
+func main() {
+	highSchoolColdLunch := LunchOption{
+		name: "High School Express Cold Lunch Menu",
+		path: "High-School-Express-Cold-Lunch-Menu",
+	}
+	config := &LunchConfig{
+		schoolYear: "2025-2026",
+		basePath:   "https://www.schools.nyc.gov/docs/default-source/school-menus/",
+		options:    []LunchOption{highSchoolColdLunch},
+	}
+
+	lunchUrl := config.UrlForOption(config.options[0])
+	resp, err := http.Get(lunchUrl)
 	if err != nil {
 		panic(err)
 	}
